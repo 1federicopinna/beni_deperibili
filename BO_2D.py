594c92fd3a1549f5dafd986150119a52f8a0facd
1federicopinna <planeut@hotmail.it>
2026-04-13 14:39:19 +0200

    Modifica dei parametri

--- BO_2D.py
+++ BO_2D.py
@@ -1,50 +1,66 @@
 from statistics import mean
 from skopt import gp_minimize
 from SimulatedAnnealing_v2 import SA, objective_function, neighbor_function, check_fidelity
+from skopt.space import Integer
 
-i = 4
-contatore = 0
+if __name__ == '__main__':
 
-
-daily_penalty = 951.91
-n_days = 80  # 20*I
-f_obj = objective_function(target_fr=0.95, daily_penalty=daily_penalty)
-f_ngh = neighbor_function(SM=23933, Sm=16417, sM=13165, sm=6567,
-                          dq=(-80, -40, -20, -10, 10, 20, 40, 80),
-                          #dq=(-20, -10, 10, 20),  # variazioni di q = (S - s)                              
-                          # dq=(-80, -40, -20, -10, 10, 20, 40, 80),
-                          # dq=(-80, -40, -20, -10, -5, -2, 2, 5, 10, 20, 40, 80),
-                          prob=(0.4, 0.4, 0.2)  # probabilità di agire solo su S, solo su s, su entrambi
-                          )
-f_fid = check_fidelity
-sa = SA(f_obj=f_obj, f_ngh=f_ngh, f_fid=f_fid, fidelity={1: 2 ** 2, 2: 2 ** 3, 3: 2 ** 4})  # creiamo l'oggetto sa
-
-seeds = sa.sds[3]
+    i = 4
+    contatore = 0
 
 
-def f(x):
-    global contatore
-    contatore += 1
-    S, s = x
-    print(f"Ciclo {contatore}: S={S}, s={s}")
-    results = sa.simulate(S, s, i=i, seeds=seeds, n_days=n_days)
-    obj_list = f_obj(results)
+    daily_penalty = 951.91
+    n_days = 80  # 20*I
+    f_obj = objective_function(target_fr=0.95, daily_penalty=daily_penalty)
+    f_ngh = neighbor_function(SM=23933, Sm=16417, sM=13165, sm=6567,
+                              dq=(-80, -40, -20, -10, 10, 20, 40, 80),
+                              #dq=(-20, -10, 10, 20),  # variazioni di q = (S - s)
+                              # dq=(-80, -40, -20, -10, 10, 20, 40, 80),
+                              # dq=(-80, -40, -20, -10, -5, -2, 2, 5, 10, 20, 40, 80),
+                              prob=(0.4, 0.4, 0.2)  # probabilità di agire solo su S, solo su s, su entrambi
+                              )
+    f_fid = check_fidelity
+    sa = SA(f_obj=f_obj, f_ngh=f_ngh, f_fid=f_fid, fidelity={1: 2 ** 2, 2: 2 ** 3, 3: 2 ** 4})  # creiamo l'oggetto sa
 
-    fo_mean = mean(obj_list)
-    print(f"Media = {fo_mean}")
-    return -fo_mean
+    seeds = sa.sds[3]
 
-# processo di regressione gaussiana - distribuzione di probabilità su funzioni
-res = gp_minimize(
-    func=f, # funzione obbiettivo da minimizzare
-    dimensions=[(16417, 23933),       # dominio di S (nuovo range)
-                (6567, 13165)],
-    acq_func="EI",  # funzione di acquisizione -  Expected Improvement
-    n_calls=80, # numero di volte che simulo per il processo di ottimizzazione
-    n_random_starts=5, # prima di iniziare campiono 5 punti casusali
-    random_state=1234, # seed per avere stessi punti iniziali
-)
 
-S_best, s_best = res.x
-fo_best = -res.fun
-print("BO 2D soluzione migliore:", S_best, s_best, "fo media migliore:", fo_best)
+    def f(x):
+        global contatore
+        contatore += 1
+        S, s = x
+        print(f"Ciclo {contatore}: S={S}, s={s}")
+        results = sa.simulate(S, s, i=i, seeds=seeds, n_days=n_days)
+        obj_list = f_obj(results)
+
+        fo_mean = mean(obj_list)
+        print(f"Media = {fo_mean}")
+        return -fo_mean
+
+    # processo di regressione gaussiana - distribuzione di probabilità su funzioni
+    #res = gp_minimize(
+    #    func=f, # funzione obbiettivo da minimizzare
+    #    dimensions=[(16417, 23933),       # dominio di S (nuovo range)
+    #                (6567, 13165)],
+    #    acq_func="EI",  # funzione di acquisizione -  Expected Improvement
+    #    n_calls=80, # numero di volte che simulo per il processo di ottimizzazione
+    #    n_random_starts=5, # prima di iniziare campiono 5 punti casusali
+    #    random_state=1234, # seed per avere stessi punti iniziali
+    #)
+
+
+    res = gp_minimize(
+        func=f,
+        dimensions=[Integer(12000, 23933, name='S'),
+                    Integer(6567, 13165, name='s')],
+        acq_func="EI",
+        n_calls=80,
+        n_random_starts=15,
+        random_state=1234,
+        n_restarts_optimizer=10,
+        noise=0.1
+    )
+
+    S_best, s_best = res.x
+    fo_best = -res.fun
+    print("BO 2D soluzione migliore:", S_best, s_best, "fo media migliore:", fo_best)
--- Simulation_con_agenti.py
+++ Simulation_con_agenti.py
@@ -11,22 +11,23 @@ from scipy.stats import norm, triang
 from Utilities import discr_cont_distrib, Theoretical_SsI_Values, Costs, Policy, CR, Prob_mass_func
 from VENDOR import Vendor
 from AGENT import Warehouse_2, Discount, Shopper, Discount_Shopper, max_sl
-from Generatore_agenti import GenP
-from parametri_questionario import  discr_min_sl, prob_guarda, discr_pop_sensibile_sconti, prob_ag_sensibile, distri_discount_acceptance, cdf_tabella_sconti, P
+
+from Generatore_agenti import GenP # classe per la generazione clientela secondo il questionario
+from parametri_questionario import  discr_min_sl, prob_guarda, prob_ag_sensibile, distri_discount_acceptance, cdf_tabella_sconti, P
 
 rn.seed(10) # tanto per far sì che la sequenza sia sempre la stessa 
 env = sp.Environment()
 
-# DISTRIBUZIONI 
+# DISTRIBUZIONI: Lead time (lt), Shelf life (disc_sl), e domanda giornaliera (dd)
 lt = triang(c=0.25, loc=1, scale=4) #shelf life triangolare continua da 1 a 5, moda 2
-disc_sl: Prob_mass_func = {13:0.1, 14:0.2 , 15:0.5, 16:0.2}  # shelf life discretizzata
+disc_sl: Prob_mass_func = {13:0.1, 14:0.2 , 15:0.5, 16:0.2}  # shelf life discreta
 dd = norm(2506, 113) # mu e sigma della domanda
 
 # COSTI
 Cst = Costs(pc = 5,  # costo
             oc = 550, # ordinazione
             dc = 0.163, # smaltimento
-            u = 0.05875,         # 0.47 / 8 ≈ 0.05875 stockout cost in frazione del prezzo
+            u = 0.05875, # 0.47 / 8 = 0.05875 stockout cost in frazione del prezzo
             h = 1.095 # %costo possesso annuo
            )
 
@@ -40,10 +41,10 @@ ideal_vals = Theoretical_SsI_Values(demand = dd,
                                     I = 4,
                                     safety_level = 0.95)
 
-print(ideal_vals)
 
-# ideal vals vecchi -> {'i': 3.16, 'S': 660, 's': 114}, {'I': 5, 'S': 846, 's': 116}  
-# valori teorici nuovi ->  {'S': 20230, 's': 10206, 'I': 4}) 
+# valori ottimi teorici per i=4 -> S: 20230, s: 10206
+# valori ottimi per BO 2D per i=4 -> S: 16417 s: 8134 fo media migliore: 501371.58875
+# valori ottimi per SA per i=4 -> S: 17566, s: 8549 fo migliore: 504065.4243423543
 Pol = Policy(s = 10206, 
             S = 20230, 
             I = 4, 
@@ -83,6 +84,11 @@ dt_mean = 3                            # media dei giorni tra due acquisti
 
 
 #### GENERAZIONE AGENTI ####
+"""
+    GENERAZIONE DELLA CLIENTELA
+        - si crea un agente "prototipo" che verrà moltiplicato 
+    
+"""
 
 agente_base = Discount_Shopper(
         env,  # env
@@ -95,7 +101,6 @@ agente_base = Discount_Shopper(
         max_sl,
         discount_acceptance={5:1}
     )
-agents: tuple[Shopper] =[]
 
 generatore = GenP(discr_min_sl, prob_guarda, prob_ag_sensibile, distri_discount_acceptance, cdf_tabella_sconti) 
 agents = generatore(P, agente_base)
@@ -116,6 +121,19 @@ B.wh.show_trend()
 #####
 results = {'rev': B.tot_revenue, 'so': B.pr_stock_out, 'fr': B.fill_rt}
 print(results)
+
+rev = float(results['rev'])
+fr = float(results['fr'])
+daily_penalty = 951.91
+target_fr = 0.95
+
+# Calcolo FO “alla SA”
+fo = rev - 100 * daily_penalty * max(0, target_fr - fr)
+
+print(f"Ricavo medio (agenti): {rev:.2f}")
+print(f"Fill rate (agenti): {fr:.5f}")
+print(f"Funzione obiettivo (agenti): {fo:.2f}")
+
 #print("n_stockout =", B.wh.n_stock_out)
 #print("N_cycles  =", len(B.order_types['i']) + len(B.order_types['s']))
 #print("pr_stockout calcolata =", B.pr_stock_out)
